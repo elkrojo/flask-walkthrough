0a389ce0df506ae22c2311f986b8e10c5d1717ee
Ronan C <rojocas@gmail.com>
2020-02-04 15:01:11 +0000

    Added jinja loops to autofill about content from json data

--- flask/run.py
+++ flask/run.py
@@ -1,25 +1,34 @@
 import os
-from flask import Flask ,render_template
+import json
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
 @app.route("/about")
 def about():
-    return render_template("about.html", page_title="About", list_of_numbers=[1, 2, 3])
+    data = []
+    with open("flask/data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title="About", company=data)
+
 
 @app.route("/contact")
 def contact():
     return render_template("contact.html", page_title="Contact")
 
+
 @app.route("/careers")
 def careers():
     return render_template("careers.html", page_title="Careers")
 
+
 if __name__ == "__main__":
-        app.run(host=os.environ.get("IP"),
-                port=int(os.environ.get("PORT")),
-                debug=True)
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
